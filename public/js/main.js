47b25edc4145a7dcc65e3aa4fdf5f92d2acad152
kevas007 <kevassiobo@gmail.com>
2021-03-03 16:11:02 +0100

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+
+function main(){
+    let max;
+    let envie;
+
+    do {
+            alert("Choisissez votre niveau \n 1 = facile : entre 1 et 10 \n 2 = moyen : entre 1 et 100 \n 3 = difficlle : entre 1 et 1000");
+       let niveau = window.prompt('Enter a value for niveau');
+        
+        // vérifier que le niveau est ok entre 1 et 3
+        while (niveau < 1 || niveau > 3) {
+            alert("Erreur : niveau inconnu ! Entrez un nombre entre 1 et 3");
+            niveau = window.prompt('Enter a value for niveau');
+        }
+        if (niveau == 1) {
+            alert("Niveau facile");
+            max = 10;
+        } else {
+            if (niveau == 2) {
+                alert("Niveau moyen");
+                max = 100;
+            } else {
+                alert("Niveau difficile");
+                max = 1000;
+            }
+        }
+        
+        let justePrix = Math.floor(Math.random() * max) + 1;
+        let nbTentatives = 0;
+        alert("Le juste prix est " + justePrix);
+        do {
+            window.alert("Entrez votre proposition");
+            proposition = window.prompt('Enter la value de la  proposition');
+            while (proposition < 1 || proposition > max) {
+                alert("Erreur : la proposition n'est pas dans les bornes entre 1 et " + max);
+                proposition = prompt('Enter la valeur de la   proposition');
+            }
+            nbTentatives = nbTentatives + 1;
+            if (proposition != justePrix) {
+                if (proposition > justePrix) {
+                    alert("C'est moins");
+                } else {
+                    alert("C'est plus");
+                }
+            }
+        } while (proposition != justePrix && nbTentatives < 10 );
+        if (proposition == justePrix) {
+            alert("C'est gagné en " + nbTentatives + " tentatives");
+        } else {
+            alert("C'est perdu ! Le juste prix était  : " + justePrix);
+        }
+        alert("Voulez-vous rejouer ? true/false");
+        envie = prompt('Enter a value for envie');
+    } while (envie == "true");
+    alert("Merci et au revoir");
+}
